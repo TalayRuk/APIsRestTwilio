cacdf305f7687e98eafb157363e24d201184c276
Jonathan Buchner <websites@jonathanbuchner.com>
2016-12-12 10:26:50 -0800

    add Nuget ResSharp.NetCore pkg

---
+++ src/APIsRestTwilio/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIsRestTwilio
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //1
+            var client = new RestClient("https://api.twilio.com/2010-04-01");
+            //2
+            var request = new RestRequest("Accounts/{{Acount SID}}/Messages", Method.POST);
+            //3
+            request.AddParameter("To", "{{recipient's phone number}}");
+            request.AddParameter("From", "{{sender's phone number}}");
+            request.AddParameter("Body", "Hello world!");
+            //4
+            client.Authenticator = new HttpBasicAuthenticator("{{Account SID}}", "{{Auth Token}}");
+            //5
+            client.ExecuteAsync(request, response =>
+            {
+                Console.WriteLine(response);
+            });
+            Console.ReadLine();
+        }
+    }
+}
